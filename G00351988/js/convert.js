8a5fa37e75d1983ec0b908a2f1b2e61850519eb7
jamiecoen7 <36043433+jamiecoen7@users.noreply.github.com>
2018-04-26 15:43:35 +0100

    buttons linked to the javascript code

--- G00351988/js/convert.js
+++ G00351988/js/convert.js
@@ -8,3 +8,37 @@ function fToM(){
   document.getElementbyId("resultsentence").innerHTML = message;
 
 }
+
+function IToC(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 2.54;
+  var message = measure + ' inches converts to ' + meters + ' centimeters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
+function YToM(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.9144;
+  var message = measure + ' yards converts to ' + meters + ' meters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
+function MIToK(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 1.60934;
+  var message = measure + ' miles converts to ' + meters + ' kilometers.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+
+}
+
